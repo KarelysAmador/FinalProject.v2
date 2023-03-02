eccd42cb7a4ecf997a44d5ece294385283565f73
Karelys Amador <karelysamador95@gmail.com>
2023-03-02 23:53:29 +0100

    Cambios-ManagerEvaluacion

--- learning-ms/src/App.js
+++ learning-ms/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import LoginMain from "./componentes/login/LoginMain.jsx";
 import AlumnoEvalucionMain from './componentes/alumnos/alumno-evaluacion/AlumnoEvaluacionMain.jsx';
 import ManagerEvaluacionGrupalMain from './componentes/manager/Manager-Evaluacion-Grupal/ManagerEvaluacionGrupalMain.jsx';
+import AutoCloseExample from './componentes/manager/Manager-Evaluacion-Grupal/botonesdropdown.jsx';
+import Table from './componentes/manager/Manager-Evaluacion-Grupal/tablaevaluacion.jsx';
+import Button from './componentes/manager/Manager-Evaluacion-Grupal/botonguardar.jsx';
+import TypesExample from './componentes/manager/Manager-Evaluacion-Grupal/botonguardar.jsx';
 
 function App() {
   return (
