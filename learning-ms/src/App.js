8c03cdaa6c6a8ba07caa48784a8c5e3d9c1353cb
Karelys Amador <karelysamador95@gmail.com>
2023-03-05 13:49:48 +0100

    Actualizacion app.js y alumnos

--- learning-ms/src/App.js
+++ learning-ms/src/App.js
@@ -3,16 +3,12 @@ import './App.css';
 import LoginMain from "./componentes/login/LoginMain.jsx";
 import AlumnoEvalucionMain from './componentes/alumnos/alumno-evaluacion/AlumnoEvaluacionMain.jsx';
 import ManagerEvaluacionGrupalMain from './componentes/manager/Manager-Evaluacion-Grupal/ManagerEvaluacionGrupalMain.jsx';
-import AutoCloseExample from './componentes/manager/Manager-Evaluacion-Grupal/botonesdropdown.jsx';
-import Table from './componentes/manager/Manager-Evaluacion-Grupal/tablaevaluacion.jsx';
-import Button from './componentes/manager/Manager-Evaluacion-Grupal/botonguardar.jsx';
-import TypesExample from './componentes/manager/Manager-Evaluacion-Grupal/botonguardar.jsx';
 import ManagerEvaluacionIndividualMain from './componentes/manager/Manager-Evaluacion-Individual/ManagerEvaluacionIndividualMain.jsx';
 
 const App = () => {
   return (
     <div className="App">
-      <LoginMain />
+      <ManagerEvaluacionIndividualMain/>
 
     </div>
   );
--- learning-ms/src/componentes/manager/Manager-Evaluacion-Individual/tablaevaluacion.jsx
+++ learning-ms/src/componentes/manager/Manager-Evaluacion-Individual/tablaevaluacion.jsx
@@ -286,6 +286,366 @@ function BasicExample() {
                 </div>
             </td>
         </tr>
+        <tr>
+         <td>JavaScript II</td>
+            <td  class> 
+                    3
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
+        <tr>
+         <td>JavaScript III</td>
+            <td  class> 
+                    1
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
+        <tr>
+         <td>JavaScript III</td>
+            <td  class> 
+                    2
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
+        <tr>
+         <td>JavaScript III</td>
+            <td  class> 
+                    3
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
+        <tr>
+         <td>React</td>
+            <td  class> 
+                    1
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
+        <tr>
+         <td>React</td>
+            <td  class> 
+                    2
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
+        <tr>
+         <td>Database</td>
+            <td  class> 
+                    1
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
+        <tr>
+         <td>DataBase</td>
+            <td  class> 
+                    2
+            </td>
+            <td  class> 
+                <div class="asistencia1">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+            </td>
+
+            <td  class> 
+                <div class="asistencia2">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Asistió</option>
+                    <option value="2">Asistió tarde CA</option>
+                    <option value="3">Asistió tarde SA</option>
+                    <option value="4">No asistió CA</option>
+                    <option value="5">No asistió SA</option>
+                    </select>
+        
+                </div>
+
+            </td>
+            <td  class> 
+                <div class="tarea">
+                    <select class="form-select" aria-label="Default select example">
+                    <option value="0"></option>
+                    <option value="1">Hecha</option>
+                    <option value="2">No hecha</option>
+                    <option value="3">Incompleta</option>
+                    </select>
+        
+                </div>
+            </td>
+        </tr>
       </tbody>
     </Table>
   );
